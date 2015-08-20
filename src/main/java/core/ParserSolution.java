69a16b93e3e53f8fc7a7a70be54bb15fd15fbe01
Stanley Alves <stanley@equalshq.com>
2015-08-20 14:10:15 -0300

    Classe que conterá a solução do problema

--- src/main/java/core/ParserSolution.java
+++ src/main/java/core/ParserSolution.java
@@ -1,5 +1,22 @@
-package solution;
+package core;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ParserSolution {
+
+	public static void main( String[] args ) throws IOException {
+		InputStream gameLogger = FindActions.class.getResourceAsStream( "/games.log" );
+		byte[] bytes = new byte[gameLogger.available()];
+		gameLogger.read( bytes );
+
+		String logerContent = new String( bytes );
+
+		Map<Integer, String> playerNameByNumber = new HashMap<>();
+		Map<Integer, String> killsByPlayerNumber = new HashMap<>();
+
+	}
 
 }
