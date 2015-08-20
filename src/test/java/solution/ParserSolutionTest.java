cd2d11df3c5bd2e33bc08dd9c43ce6305548c270
Stanley Alves <stanley@equalshq.com>
2015-08-20 17:44:11 -0300

    Inclusão de teste para expressão regular que captura as ações no log

--- src/test/java/solution/ParserSolutionTest.java
+++ src/test/java/solution/ParserSolutionTest.java
@@ -1,5 +1,41 @@
 package solution;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.junit.Assert;
+
+import core.FindActions;
+
 public class ParserSolutionTest {
+
+	private String getLoggerContent() throws IOException {
+		InputStream gameLogger = FindActions.class.getResourceAsStream( "/games.log" );
+		byte[] bytes = new byte[gameLogger.available()];
+		gameLogger.read( bytes );
+
+		return new String( bytes );
+	}
+
+	public void actionRegexTest() throws IOException {
+
+		String logerContent = getLoggerContent();
+
+		Pattern pattern = Pattern.compile( " \\d{2}:\\d{2}\\s(\\w+):.*" );
+		Matcher matcher = pattern.matcher( logerContent );
+
+		Set<String> actions = new HashSet<>();
+
+		while ( matcher.find() ) {
+			actions.add( matcher.group( 1 ) );
+		}
+
+		Assert.assertTrue( !actions.isEmpty() );
+
+	}
 
 }
