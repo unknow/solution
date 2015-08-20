55be6ee141049b21b657cd97a3d6650c96adbe34
Stanley Alves <stanley@equalshq.com>
2015-08-20 17:45:57 -0300

    Classe main que varre o arquivo para contabilização das kills

--- src/main/java/core/ParserSolution.java
+++ src/main/java/core/ParserSolution.java
@@ -2,8 +2,12 @@ package core;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import enums.ActionType;
 
 public class ParserSolution {
 
@@ -14,8 +18,81 @@ public class ParserSolution {
 
 		String logerContent = new String( bytes );
 
-		Map<Integer, String> playerNameByNumber = new HashMap<>();
-		Map<Integer, String> killsByPlayerNumber = new HashMap<>();
+		String actionRegex = " \\d{1,2}:\\d{2}\\s(\\w+):(.*)";
+
+		Pattern pattern = Pattern.compile( actionRegex );
+		Matcher matcher = pattern.matcher( logerContent );
+
+		String action = null;
+		Game game = null;
+
+		List<Game> games = new ArrayList<>();
+
+		while ( matcher.find() ) {
+			action = matcher.group( 1 );
+			ActionType actionType = ActionType.getByDescription( action );
+
+			// se for uma ação válida para resolução do problema
+			if ( actionType != null ) {
+
+				// de acordo com a ação efetuar operações
+				switch ( actionType ) {
+					case INIT_GAME:
+
+						game = new Game();
+						games.add( game );
+						break;
+
+					case KILL:
+						if ( game != null ) {
+
+							String moreInfos = matcher.group( 2 );
+
+							Pattern patternMoreInfos = Pattern.compile( "(\\d+)\\s(\\d+)\\s(\\d+):\\s(.+)\\skilled\\s(.+)?\\sby\\s(.+)" );
+							Matcher matcherMoreInfos = patternMoreInfos.matcher( moreInfos );
+
+							if ( matcherMoreInfos.find() ) {
+
+								int playerKillerId = Integer.valueOf( matcherMoreInfos.group( 1 ) );
+								int playerDeathId = Integer.valueOf( matcherMoreInfos.group( 2 ) );
+								int causeDeathId = Integer.valueOf( matcherMoreInfos.group( 3 ) );
+
+								String playerKillerName = matcherMoreInfos.group( 4 );
+								String playerDeathName = matcherMoreInfos.group( 5 );
+								String causeDeathDescription = matcherMoreInfos.group( 6 );
+
+								// adiciona jogador que matou
+								game.addPlayer( playerKillerId, playerKillerName );
+
+								// adiciona jogador que morreu
+								game.addPlayer( playerDeathId, playerDeathName );
+
+								// adicionar causa da morte
+								game.addDeathCause( causeDeathId, causeDeathDescription );
+
+								// contabiliza a
+								game.addKill( playerKillerId, playerDeathId, causeDeathId );
+							}
+						} else {
+							throw new IllegalArgumentException( "Game não inicializado !!!" );
+						}
+
+						break;
+
+					case SHUTDOWN_GAME:
+						// há jogos que não estão finalizados no log, optei por
+						// não utilizar esta ação
+						break;
+
+					default:
+						break;
+				}
+			}
+		}
+
+		for ( Game currentGame : games ) {
+			System.out.println( currentGame.toString() );
+		}
 
 	}
 
